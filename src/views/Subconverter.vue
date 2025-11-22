9689c53b8fa6c662825edd90c6810a8224c81b0b
0x1233333 <54734393+0x1233333@users.noreply.github.com>
2025-11-22 20:49:58 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,13 +339,6 @@ export default {
         },
         customBackend: {
           "自用": "https://subconverter.0x0000.top",
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
-          "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
@@ -1315,5 +1308,6 @@ export default {
   }
 };
 </script>
+
 
 
